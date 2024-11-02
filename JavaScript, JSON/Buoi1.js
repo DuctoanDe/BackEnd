b22712e11584d2aa4bf6a41ad3b3628268f0a149
NDTOANDE <ndtoande@gmail.com>
2024-11-03 04:33:35 +0700

    Js_Buoi1

---
+++ JavaScript, JSON/Buoi1.js
@@ -0,0 +1,12 @@
+
+function myFunction() {
+    document.getElementById("Demo").innerHTML = "Sure";
+  }
+//Hộp cảnh báo lỗi
+window.alert("Cẩn thận với dữ liệu này!")  
+//Gỡ lỗi
+console.log("Cẩn thận với dữ liệu này")
+//Print (in)
+window.print()
+
+
